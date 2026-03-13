d0be970e2fc8f9c57ed30582e6a9cdb176d41611
pepecarbajal <prbsPepe@gmail.com>
2026-03-13 17:53:50 -0600

    eliminado incompativilidad con vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mapbox':  ['mapbox-gl'],
+          'react':   ['react', 'react-dom'],
+          'xlsx':    ['xlsx'],
+        }
+      }
+    }
   }
-});
+})
